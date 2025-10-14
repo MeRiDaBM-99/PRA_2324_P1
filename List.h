59f3440910b4e63e7f36256e8e4a42b7aeefbc18
Marcos Merida Bustamante <marmerbus06@gmail.com>
2025-10-14 22:38:43 +0200

    Modificación pequeña en List.h

--- List.h
+++ List.h
@@ -6,16 +6,16 @@ using namespace std;
 template <typename T>
 class List{
     public:
-        virtual void insert(int pos, T e) = 0;
-        virtual void append(T e) = 0;
-        virtual void prepend(T e) = 0;
+        virtual void insert(int pos, T e) = 0;  //Añadir e en pos
+        virtual void append(T e) = 0;           //Insertar e al final
+        virtual void prepend(T e) = 0;          //Insertar e al principio
 
-        virtual T remove(int pos) = 0;
-        virtual T get (int pos) = 0;
+        virtual T remove(int pos) = 0;          //Eliminar el elemento en pos (y lo devuelve)
+        virtual T get (int pos) = 0;            //Devuelve el elelemento en pos
 
-        virtual int search(T e) = 0;
-        virtual bool empty() = 0;
-        virtual int size() = 0;
+        virtual int search(T e) = 0;            //Devuelve la posicion de e (sino -1)
+        virtual bool empty() = 0;               //Indicar si esta vacio
+        virtual int size() = 0;                 //Nº elementos de la lista
 	
 	virtual ~List(){}
 };
